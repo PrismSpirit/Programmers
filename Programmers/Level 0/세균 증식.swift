2479a84dab726108e7f208c76f1fdd5e24229f8a
PrismSpirit <yeolkeut@gmail.com>
2023-01-13 22:16:41 +0900

    Solve 세균 증식

---
+++ Programmers/Level 0/세균 증식.swift
@@ -0,0 +1,17 @@
+//
+//  세균 증식.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120910
+//
+//  Created by Jaehun Lee on 2023/01/13.
+//
+
+import Foundation
+
+func solution(_ n: Int, _ t: Int) -> Int {
+    return n << t
+}
+
+
+print(solution(2, 10))
+print(solution(7, 15))
