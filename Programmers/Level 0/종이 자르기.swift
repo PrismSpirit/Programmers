ccff2b369b3dbe78c649f9d1ca636f577139d7ee
PrismSpirit <yeolkeut@gmail.com>
2023-01-13 16:48:07 +0900

    Solve 종이 자르기

---
+++ Programmers/Level 0/종이 자르기.swift
@@ -0,0 +1,18 @@
+//
+//  종이 자르기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120922
+//
+//  Created by Jaehun Lee on 2023/01/13.
+//
+
+import Foundation
+
+func solution(_ M: Int, _ N: Int) -> Int {
+    return M * N - 1
+}
+
+
+print(solution(2, 2))
+print(solution(2, 5))
+print(solution(1, 1))
