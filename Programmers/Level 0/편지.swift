e9c447cc7a9f1378dc9e935d91bba7663d592739
PrismSpirit <yeolkeut@gmail.com>
2023-01-18 03:22:35 +0900

    Solve 편지

---
+++ Programmers/Level 0/편지.swift
@@ -0,0 +1,17 @@
+//
+//  편지.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120898
+//
+//  Created by Jaehun Lee on 2023/01/18.
+//
+
+import Foundation
+
+func solution(_ message: String) -> Int {
+    return message.count << 1
+}
+
+
+print(solution("happy birthday!"))
+print(solution("I love you~"))
