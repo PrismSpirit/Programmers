1c660f70d55bd144c8d9a74f93f88689ced414b0
PrismSpirit <yeolkeut@gmail.com>
2023-01-20 01:33:55 +0900

    Solve 문자열 뒤집기

---
+++ Programmers/Level 0/문자열 뒤집기.swift
@@ -0,0 +1,17 @@
+//
+//  문자열 뒤집기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120822
+//
+//  Created by Jaehun Lee on 2023/01/20.
+//
+
+import Foundation
+
+func solution(_ my_string: String) -> String {
+    return String(my_string.reversed())
+}
+
+
+print(solution("jaron"))
+print(solution("bread"))
