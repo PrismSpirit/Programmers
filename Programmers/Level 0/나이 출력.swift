afcbe3f951cb12b02de1ac59b903647f8a75b519
PrismSpirit <yeolkeut@gmail.com>
2023-01-20 01:37:32 +0900

    Solve 나이 출력

---
+++ Programmers/Level 0/나이 출력.swift
@@ -0,0 +1,17 @@
+//
+//  나이 출력.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120820
+//
+//  Created by Jaehun Lee on 2023/01/20.
+//
+
+import Foundation
+
+func solution(_ age: Int) -> Int {
+    return 2023 - age
+}
+
+
+print(solution(40))
+print(solution(23))
