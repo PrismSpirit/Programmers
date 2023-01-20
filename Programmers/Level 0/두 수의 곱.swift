98e40ef71caa42024243e0a1e31b33618aa49a1e
PrismSpirit <yeolkeut@gmail.com>
2023-01-20 18:44:11 +0900

    Solve 두 수의 곱

---
+++ Programmers/Level 0/두 수의 곱.swift
@@ -0,0 +1,17 @@
+//
+//  두 수의 곱.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120804
+//
+//  Created by Jaehun Lee on 2023/01/20.
+//
+
+import Foundation
+
+func solution(_ num1: Int, _ num2: Int) -> Int {
+    return num1 * num2
+}
+
+
+print(solution(3, 4))
+print(solution(27, 19))
