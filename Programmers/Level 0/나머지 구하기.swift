ac2e6ad474e4fe9cd51c25939f93144cb881051f
PrismSpirit <yeolkeut@gmail.com>
2023-01-20 18:22:56 +0900

    Solve 나머지 구하기

---
+++ Programmers/Level 0/나머지 구하기.swift
@@ -0,0 +1,17 @@
+//
+//  나머지 구하기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120810
+//
+//  Created by Jaehun Lee on 2023/01/20.
+//
+
+import Foundation
+
+func solution(_ num1: Int, _ num2: Int) -> Int {
+    return num1 % num2
+}
+
+
+print(solution(3, 2))
+print(solution(10, 5))
