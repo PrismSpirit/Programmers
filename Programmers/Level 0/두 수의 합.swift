647b9dbfadfcb1b9164721c52f10b765645d8c35
PrismSpirit <yeolkeut@gmail.com>
2023-01-20 18:47:19 +0900

    Solve 두 수의 합

---
+++ Programmers/Level 0/두 수의 합.swift
@@ -0,0 +1,17 @@
+//
+//  두 수의 합.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120802
+//
+//  Created by Jaehun Lee on 2023/01/20.
+//
+
+import Foundation
+
+func solution(_ num1: Int, _ num2: Int) -> Int {
+    return num1 + num2
+}
+
+
+print(solution(2, 3))
+print(solution(100, 2))
