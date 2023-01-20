9bd4e89f6d330e648b1adf182b21ef7f743c5b54
PrismSpirit <yeolkeut@gmail.com>
2023-01-20 18:42:38 +0900

    Solve 몫 구하기

---
+++ Programmers/Level 0/몫 구하기.swift
@@ -0,0 +1,17 @@
+//
+//  몫 구하기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120805
+//
+//  Created by Jaehun Lee on 2023/01/20.
+//
+
+import Foundation
+
+func solution(_ num1: Int, _ num2: Int) -> Int {
+    return num1 / num2
+}
+
+
+print(solution(10, 5))
+print(solution(7, 2))
