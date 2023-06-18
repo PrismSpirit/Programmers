befee8f1b00744f938854c40411a461ec8faec67
PrismSpirit <yeolkeut@gmail.com>
2023-06-18 19:54:39 +0900

    Solve 소문자로 바꾸기

---
+++ Programmers/Level 0/소문자로 바꾸기.swift
@@ -0,0 +1,17 @@
+//
+//  소문자로 바꾸기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181876
+//
+//  Created by Jaehun Lee on 2023/06/18.
+//
+
+import Foundation
+
+func solution(_ myString: String) -> String {
+    return myString.lowercased()
+}
+
+
+print(solution("aBcDeFg"))
+print(solution("aaa"))
