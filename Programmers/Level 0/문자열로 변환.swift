71c9ae5c6220f1d3a5f40f1171fe4c865bc66a93
PrismSpirit <yeolkeut@gmail.com>
2023-06-18 23:25:15 +0900

    Solve 문자열로 변환

---
+++ Programmers/Level 0/문자열로 변환.swift
@@ -0,0 +1,17 @@
+//
+//  문자열로 변환.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181845
+//
+//  Created by Jaehun Lee on 2023/06/18.
+//
+
+import Foundation
+
+func solution(_ n: Int) -> String {
+    return String(n)
+}
+
+
+print(solution(123))
+print(solution(2573))
