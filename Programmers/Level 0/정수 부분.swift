bdec50267de321de3a1be3ba8c8161b0099ca2c1
PrismSpirit <yeolkeut@gmail.com>
2023-06-18 23:12:20 +0900

    Solve 정수 부분

---
+++ Programmers/Level 0/정수 부분.swift
@@ -0,0 +1,17 @@
+//
+//  정수 부분.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181850
+//
+//  Created by Jaehun Lee on 2023/06/18.
+//
+
+import Foundation
+
+func solution(_ flo: Double) -> Int {
+    return Int(flo)
+}
+
+
+print(solution(1.42))
+print(solution(69.32))
