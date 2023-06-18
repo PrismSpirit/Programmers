abe087c41db7dea5940bed07cd27b029b94462db
PrismSpirit <yeolkeut@gmail.com>
2023-06-18 23:28:24 +0900

    Solve 카운트 업

---
+++ Programmers/Level 0/카운트 업.swift
@@ -0,0 +1,16 @@
+//
+//  카운트 업.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181920
+//
+//  Created by Jaehun Lee on 2023/06/18.
+//
+
+import Foundation
+
+func solution(_ start: Int, _ end: Int) -> [Int] {
+    return Array(start...end)
+}
+
+
+print(solution(3, 10))
