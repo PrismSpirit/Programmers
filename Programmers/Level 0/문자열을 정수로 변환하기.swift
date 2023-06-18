ab39f81ff82a9c9388d36d15b660f0933f3f1da9
PrismSpirit <yeolkeut@gmail.com>
2023-06-18 23:27:01 +0900

    Solve 문자열을 정수로 변환하기

---
+++ Programmers/Level 0/문자열을 정수로 변환하기.swift
@@ -0,0 +1,17 @@
+//
+//  문자열을 정수로 변환하기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181848
+//
+//  Created by Jaehun Lee on 2023/06/18.
+//
+
+import Foundation
+
+func solution(_ n_str: String) -> Int {
+    return Int(n_str)!
+}
+
+
+print(solution("10"))
+print(solution("8542"))
