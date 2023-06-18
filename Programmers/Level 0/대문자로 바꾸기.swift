e956f9301e06f3c9a2616e6251117716673fffee
PrismSpirit <yeolkeut@gmail.com>
2023-06-18 23:23:53 +0900

    Solve 대문자로 바꾸기

---
+++ Programmers/Level 0/대문자로 바꾸기.swift
@@ -0,0 +1,17 @@
+//
+//  대문자로 바꾸기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181877
+//
+//  Created by Jaehun Lee on 2023/06/18.
+//
+
+import Foundation
+
+func solution(_ myString: String) -> String {
+    return myString.uppercased()
+}
+
+
+print(solution("aBcDeFg"))
+print(solution("AAA"))
