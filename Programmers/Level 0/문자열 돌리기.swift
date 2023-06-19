6453217ea3b797526bdc908aa3e6eb06d9991761
PrismSpirit <yeolkeut@gmail.com>
2023-06-20 05:16:06 +0900

    Solve 문자열 돌리기

---
+++ Programmers/Level 0/문자열 돌리기.swift
@@ -0,0 +1,15 @@
+//
+//  문자열 돌리기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181945
+//
+//  Created by Jaehun Lee on 2023/06/20.
+//
+
+import Foundation
+
+let s1 = readLine()!
+
+for c in s1 {
+    print(c)
+}
