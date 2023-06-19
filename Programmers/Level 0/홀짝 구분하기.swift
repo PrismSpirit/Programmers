c4069fee79c67e23e0eee43bafe7d9f2e6cf7d3b
PrismSpirit <yeolkeut@gmail.com>
2023-06-20 02:02:21 +0900

    Solve 홀짝 구분하기

---
+++ Programmers/Level 0/홀짝 구분하기.swift
@@ -0,0 +1,13 @@
+//
+//  홀짝 구분하기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181944
+//
+//  Created by Jaehun Lee on 2023/06/20.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+
+print("\(a) is \(a % 2 != 0 ? "odd" : "even")")
