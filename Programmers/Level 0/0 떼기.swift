616951d38c5498c5336b97febceaa23cc81e218b
PrismSpirit <yeolkeut@gmail.com>
2023-06-20 03:19:49 +0900

    Solve 0 떼기

---
+++ Programmers/Level 0/0 떼기.swift
@@ -0,0 +1,17 @@
+//
+//  0 떼기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181847
+//
+//  Created by Jaehun Lee on 2023/06/20.
+//
+
+import Foundation
+
+func solution(_ n_str: String) -> String {
+    return String(Int(n_str)!)
+}
+
+
+print(solution("0010"))
+print(solution("854020"))
