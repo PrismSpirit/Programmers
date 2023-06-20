5e932cbf5b7b775a64d6c1d982cc2d91eb7ab096
PrismSpirit <yeolkeut@gmail.com>
2023-06-21 02:09:57 +0900

    Solve 문자열 출력하기

---
+++ Programmers/Level 0/문자열 출력하기.swift
@@ -0,0 +1,13 @@
+//
+//  문자열 출력하기.swift
+//  Programmers
+//  https://school.programmers.co.kr/learn/courses/30/lessons/181952
+//
+//  Created by Jaehun Lee on 2023/06/21.
+//
+
+import Foundation
+
+let s1 = readLine()!
+
+print(s1)
